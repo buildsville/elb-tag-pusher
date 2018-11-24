28fed04a878c8b99efb2c8051344271baa6f962a
masahata <buildsville.lab@gmail.com>
2018-11-24 10:26:19 +0900

    go fmt

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"flag"
-  "regexp"
-  "time"
+	"regexp"
+	"time"
 
-  "github.com/prometheus/common/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
+	"github.com/prometheus/common/log"
 
-  "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elb"
 )
 
 const (
-  defaultPushIntervalSec = 60
-  defaultPushGateWayAddr = "http://localhost:9091"
-  defaultJobName = "push_elb_tag"
-  defaultMetricsName = "aws_elb_tags"
-  defalutELBNameLabelKey = "load_balancer_name"
-	defaultReplaceChar = "/"
+	defaultPushIntervalSec = 60
+	defaultPushGateWayAddr = "http://localhost:9091"
+	defaultJobName         = "push_elb_tag"
+	defaultMetricsName     = "aws_elb_tags"
+	defalutELBNameLabelKey = "load_balancer_name"
+	defaultReplaceChar     = "/"
 )
 
 var pushAddr = flag.String("pushGateWayAddr", defaultPushGateWayAddr, "push metrics gateway address.")
@@ -27,7 +27,7 @@ var interval = flag.Int("pushInterval", defaultPushIntervalSec, "Interval to pus
 var jobName = flag.String("jobName", defaultJobName, "job name of metrics.")
 var metricsName = flag.String("metricsName", defaultMetricsName, "metrics name of elb tag info.")
 var elbNameLabelKey = flag.String("elbNameLabelKey", defalutELBNameLabelKey, "the key of elb name label.")
-var replace = flag.String("replaceChar",defaultReplaceChar, "a character to replace unusable characters in labels.")
+var replace = flag.String("replaceChar", defaultReplaceChar, "a character to replace unusable characters in labels.")
 
 var elbSession = func() *elb.ELB {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -36,54 +36,54 @@ var elbSession = func() *elb.ELB {
 	return elb.New(sess)
 }()
 
-func getTagDescriptions() ([]*elb.TagDescription,error) {
-  var elbNames []*string
-  input := &elb.DescribeLoadBalancersInput{}
-  ret,err := elbSession.DescribeLoadBalancers(input)
-  if err != nil {
-    return nil , err
-  }
-  for _,lb := range ret.LoadBalancerDescriptions{
-    elbNames = append(elbNames,lb.LoadBalancerName)
-  }
-  var tags []*elb.TagDescription
-  for _,elbn := range elbNames {
-    taginput := &elb.DescribeTagsInput{
-      LoadBalancerNames: []*string{elbn},
-    }
-    tag,err := elbSession.DescribeTags(taginput)
-    if err != nil {
-      return nil , err
-    }
-    tags = append(tags,tag.TagDescriptions...)
-  }
-  return tags,nil
+func getTagDescriptions() ([]*elb.TagDescription, error) {
+	var elbNames []*string
+	input := &elb.DescribeLoadBalancersInput{}
+	ret, err := elbSession.DescribeLoadBalancers(input)
+	if err != nil {
+		return nil, err
+	}
+	for _, lb := range ret.LoadBalancerDescriptions {
+		elbNames = append(elbNames, lb.LoadBalancerName)
+	}
+	var tags []*elb.TagDescription
+	for _, elbn := range elbNames {
+		taginput := &elb.DescribeTagsInput{
+			LoadBalancerNames: []*string{elbn},
+		}
+		tag, err := elbSession.DescribeTags(taginput)
+		if err != nil {
+			return nil, err
+		}
+		tags = append(tags, tag.TagDescriptions...)
+	}
+	return tags, nil
 }
 
 func main() {
-  flag.Parse()
-  keyReg := regexp.MustCompile(`[ -/:-@{-~]`)
-  valReg := regexp.MustCompile(`/`)
-  for{
-    tagDescs,err := getTagDescriptions()
-    if err != nil {
-      log.Errorln(err)
-    }
-  	awsElbTags := prometheus.NewGauge(prometheus.GaugeOpts{
-  		Name: *metricsName,
-  		Help: "tag key and value on aws elastic load balancer.",
-  	})
-  	awsElbTags.Set(float64(1))
-    for _, tagDesc := range tagDescs {
-      push := push.New(*pushAddr,*jobName).Collector(awsElbTags)
-      push.Grouping(*elbNameLabelKey,*tagDesc.LoadBalancerName)
-      for _, t := range tagDesc.Tags {
-        push.Grouping(keyReg.ReplaceAllString(*t.Key, *replace),valReg.ReplaceAllString(*t.Value,*replace))
-      }
-      if err := push.Push(); err != nil {
-        log.Errorf("Could not push completion time to Pushgateway:", err)
-      }
-    }
-    time.Sleep(time.Duration(*interval) * time.Second)
-  }
+	flag.Parse()
+	keyReg := regexp.MustCompile(`[ -/:-@{-~]`)
+	valReg := regexp.MustCompile(`/`)
+	for {
+		tagDescs, err := getTagDescriptions()
+		if err != nil {
+			log.Errorln(err)
+		}
+		awsElbTags := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: *metricsName,
+			Help: "tag key and value on aws elastic load balancer.",
+		})
+		awsElbTags.Set(float64(1))
+		for _, tagDesc := range tagDescs {
+			push := push.New(*pushAddr, *jobName).Collector(awsElbTags)
+			push.Grouping(*elbNameLabelKey, *tagDesc.LoadBalancerName)
+			for _, t := range tagDesc.Tags {
+				push.Grouping(keyReg.ReplaceAllString(*t.Key, *replace), valReg.ReplaceAllString(*t.Value, *replace))
+			}
+			if err := push.Push(); err != nil {
+				log.Errorf("Could not push completion time to Pushgateway:", err)
+			}
+		}
+		time.Sleep(time.Duration(*interval) * time.Second)
+	}
 }
